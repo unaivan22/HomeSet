636675b0a7e3417f02c67b0dd3fd4f158b7736d4
unaivan <unaivan@gmail.com>
2023-07-17 08:27:17 +0700

    Initial Commit

---
+++ HomeSet/HomeSetApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeSetApp.swift
+//  HomeSet
+//
+//  Created by una ivan on 17/07/23.
+//
+
+import SwiftUI
+
+@main
+struct HomeSetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
